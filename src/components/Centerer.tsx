996ec0bd60d6366e7057db71b63514839ba62167
Ho-no <Hono_sama@qq.com>
2022-11-05 17:31:02 +0800

    feat: init components

---
+++ src/components/Centerer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Centered: React.FC = () => {
+  return (
+    <>
+      <div className='container'>
+        <div className='block'>
+          <div className='centered one'></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Centered
